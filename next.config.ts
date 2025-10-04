7583ec99fbe4bbcd63acbeb645cb3204229aa335
Ghaith00 <ghaith4816b@gmail.com>
2025-10-04 23:22:33 +0200

    fix(build): remove standalone next building

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
-const prodconfig = process.env.NODE_ENV === 'production' && { output: 'standalone' };
-
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default { ...nextConfig, ...prodconfig };
+export default nextConfig;
