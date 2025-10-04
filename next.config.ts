61a357aea1727eabdfe532f06c49f93194b70d9e
Ghaith00 <ghaith4816b@gmail.com>
2025-10-04 16:58:48 +0200

    fix(build): update docker prod image

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
+const prodconfig = process.env.NODE_ENV === 'production' && { output: 'standalone' };
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default { ...nextConfig, ...prodconfig };
